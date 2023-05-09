7b21da5273b80ac7459880f44c8d95e5f7dbb5bc
Ambraaaliu <cl6376@nyu.edu>
2023-05-09 20:24:06 +0800

    sonnyversdion

--- ProjectB/pastclicker.js
+++ ProjectB/pastclicker.js
@@ -1,27 +1,30 @@
 const images=[
-    "pastimg1.png","pastimg2.png","pastimg3.png","pastimg4.png",
-    "pastimg5.png","pastimg6.png","pastimg7.png","pastimg8.png"
+    "pastImg1.png","pastImg2.png","pastImg3.png","pastImg4.png",
+    "pastImg5.png","pastImg6.png","pastImg7.png","pastImg8.png"
 ]
 
 let i = 0
 
 function placeImage(x, y) {
     const nextSrc = images[i]
+
+
+      const img = document.createElement("img")
+      img.setAttribute("src", nextSrc)
+      img.setAttribute("draggable", "false")
+      
+      img.style.left = x + "px"
+      img.style.top = y + "px"
+      img.style.transform = "translate(-50%, -50%) scale(0.5) rotate(" + (Math.random() * 20 - 10) + "deg)"
+      img.style.zIndex = "1";
+      
+      document.body.appendChild(img)
+      
+      i = i + 1
+      if (i >= images.length) {
+        i = 0
+      }
     
-    const img = document.createElement("img")
-    img.setAttribute("src", nextSrc)
-    img.setAttribute("draggable", "false")
-    
-    img.style.left = x + "px"
-    img.style.top = y + "px"
-    img.style.transform = "translate(-50%, -50%) scale(0.5) rotate(" + (Math.random() * 20 - 10) + "deg)"
-    
-    document.body.appendChild(img)
-    
-    i = i + 1
-    if (i >= images.length) {
-      i = 0
-    }
   }
   
   document.addEventListener("click", function (event) {
